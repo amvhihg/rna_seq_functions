5fa187e4a80d9f8dc7ee163961ce83d34aa3cbf5
amvhihg <axv851@miami.edu>
2022-12-12 14:45:41 -0500

    Initial functions without comments for sva_cleaning_mod and gene_name_converter

---
+++ rna_seq_functions.R
@@ -0,0 +1,91 @@
+#' Calculate pca decomposition of an expression matrix and graph a scatter plot of pca values colored by phenotype of choice
+#' 
+#' @param expr_m an normalized and variance stabilized expression matrix
+#' @param exp_var A character signifying the name of the phenotype used for coloring scatterplot points in the pca plot
+#' 
+#' @return list containing 1.) scatter plot of the first two principal components 2.) output from the functino prcomp on the expression matrix 3.)
+#' mean and standard deviations of the first two principal components 4.) A dataframe containing the first two principal components and the phenotype of interest
+
+pca_data_out <- function(expr_m, exp_var){
+  
+
+  n_cols            <- ncol(expr_m) - 3 # Final two columns contain the expression matrix
+  
+  gene_exp_mat  <- t(as.matrix(expr_m[,1:n_cols]))  # gene expression matrix
+  gene_exp_mat  <- apply(gene_exp_mat,2,as.numeric) # convert the values to numeric 
+  gene_exp_mat  <- t(gene_exp_mat)                  # samples as columns and features as rows  
+  pca           <- prcomp( gene_exp_mat, center = TRUE, scale. = TRUE ) # build pca object
+  pca_df        <- as.data.frame(cbind(pca$x[,1],pca$x[,2],expr_m[exp_var])) # create a dataframe with the first two principal components and the phenotype variable
+  colnames(pca_df) <- c("PC1", "PC2","status") 
+  pca_df$status <- as.factor(pca_df$status)
+  pca_df$pat    <- rownames(pca_df)
+  # create a ggplot object with the phenotype column as the color 
+  hor1          <- sd(pca$x[,1])
+  hor2          <- sd(pca$x[,2])
+  ver1          <- mean(pca$x[,1])
+  ver2          <- mean(pca$x[,2])
+  exp_pca       <- ggplot(pca_df, aes(x = PC1,y = PC2, label = rownames(pca_df))) + geom_point(aes(color = status)) 
+  exp_pca       <- exp_pca + geom_vline( xintercept = ver1 + 3*hor1 ) + geom_vline( xintercept = ver1 - 3*hor1)    + geom_text_repel()# add vertical lines to cover +- 3 standard deviations
+  exp_pca       <- exp_pca + geom_hline( yintercept = ver2 + 3*hor2)  +  geom_hline(yintercept = ver2 - 3*hor2) # add horizontal lines to cover +-3 standard deviations
+
+  
+  
+  return(list(exp_pca, pca,list(hor1,hor2,ver1,ver2),pca_df))
+  
+}
+#' Function to estimate surrogate variable values from a rnaseq generated expression matrix
+#' 
+#' @param edat A dataframe with transpose of an expression matrix merged with case, sex and age
+#' 
+#' @return list composed of output from svaseq function from package "sva"
+sva_cleaning_age <- function(edat){
+
+
+  mod_data   <- model.matrix( ~1 + case + sex+ case *sex + age + age*sex, data = edat)
+  mod0       <- model.matrix(~1  +sex + age , data = edat)
+  n_cols     <- ncol(edat) - 3
+  p_cols     <- ncol(edat) - 1
+  expr_ae_ad <- apply(edat[,1:n_cols],2,as.double)
+  #expr_ae_ad <- as.matrix(eset_ae_ad_pb[,1:2063])
+  #expr_ae_ad <- apply(expr_ae_ad,2,as.numeric)
+  
+  n.survar   <- num.sv(t(expr_ae_ad),mod_data, method = "leek") 
+  
+  svobj  <- svaseq(t(expr_ae_ad), mod_data, mod0,numSVmethod = "leek")
+  
+  return(svobj)
+}
+sva_cleaning_mod <- function(edat){
+  # edat is the expression matrix with sex and case as the last two columns 
+  mod_data   <- model.matrix( ~1 + case + sex+ case *sex, data = edat)
+  mod0       <- model.matrix(~1  +sex  , data = edat)
+  n_cols     <- ncol(edat) - 3
+  p_cols     <- ncol(edat) - 1
+  expr_ae_ad <- apply(edat[,1:n_cols],2,as.double)
+  #expr_ae_ad <- as.matrix(eset_ae_ad_pb[,1:2063])
+  #expr_ae_ad <- apply(expr_ae_ad,2,as.numeric)
+  
+  n.survar   <- num.sv(t(expr_ae_ad),mod_data, method = "leek") 
+  
+  svobj  <- svaseq(t(expr_ae_ad), mod_data, mod0,numSVmethod = "leek")
+  
+  return(svobj)
+}
+
+
+
+
+
+gene_name_converter <- function(genes)
+  
+{
+  mart <- useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
+
+  ensembl_ids <- sub("[.][0-9]*","", genes)
+  gene_ids <- getBM(filters = "ensembl_gene_id", attributes = c("ensembl_gene_id", "hgnc_symbol","chromosome_name"), values = ensembl_ids, mart = mart)
+ 
+  
+  return(gene_ids)
+  
+}
+
